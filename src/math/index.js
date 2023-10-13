91ee2b1da51427c3269e60b7679d81e0cf7b460f
kaadon.com <kaadon.com@gmail.com>
2023-10-13 13:47:26 +0800

    first commit

--- src/math/index.js
+++ src/math/index.js
@@ -1,3 +1,30 @@
+import { BigNumber } from 'bignumber.js';
+
 export function MathRound(float, num) {
     return Math.round(float * Math.pow(10, num)) / Math.pow(10, num)
+}
+
+// +
+export function add(a, b) {
+    a = BigNumber(a);
+    b = BigNumber(b);
+    return a.plus(b).toNumber();   //结果需要用toNumber转为普通数字
+}
+// -
+export function minus(a, b) {
+    a = BigNumber(a);
+    b = BigNumber(b);
+    return a.minus(b).toNumber();
+}
+// *
+export function mut(a, b) {
+    a = BigNumber(a);
+    b = BigNumber(b);
+    return a.multipliedBy(b).toNumber();
+}
+// /
+export function dev(a, b) {
+    a = BigNumber(a);
+    b = BigNumber(b);
+    return a.dividedBy(b).toNumber();
 }
--- src/string/index.js
+++ src/string/index.js
@@ -9,3 +9,15 @@ export function replaceAll(text, repstr, newstr) {
 export function stringTrim(value) {
     return value.replace(/(^\s*)|(\s*$)/g, "");
 }
+
+export function toBase64(str) {
+    if (typeof str === "object") str = JSON.stringify(str)
+    if (typeof str === "number") str = str.toString()
+    return Buffer.from(str).toString("base64")
+}
+
+export function fromBase64(str) {
+    if (str === "0") return
+    if (typeof str === "string") return JSON.parse(Buffer.from(str, 'base64').toString("utf8"))
+    throw new Error("数据错误!")
+}
