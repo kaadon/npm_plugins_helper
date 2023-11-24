249e5bcc3c125c67f43aeb9729e79f48bdcd797e
kaadon.com <kaadon.com@gmail.com>
2023-11-24 14:37:13 +0800

    first commit

--- src/math/index.js
+++ src/math/index.js
@@ -1,4 +1,6 @@
 import { BigNumber } from 'bignumber.js';
+import fs from "fs";
+import crypto from "crypto";
 
 export function MathRound(float, num) {
     return Math.round(float * Math.pow(10, num)) / Math.pow(10, num)
@@ -27,4 +29,27 @@ export function dev(a, b) {
     a = BigNumber(a);
     b = BigNumber(b);
     return a.dividedBy(b).toNumber();
+}
+
+
+export const fileCp = (oldPath,newPath) => {
+    try {
+        fs.renameSync(oldPath, newPath);
+    } catch (err) {
+        throw err
+    }
+}
+export const randomIntFromInterval =( min, max) =>  {
+    const rangeNumber = max - min;
+    if (rangeNumber < 0) {
+        throw new Error('最大值必须大于最小值');
+    }
+    const bytesNeeded = Math.ceil(Math.log2(rangeNumber + 1) / 8);
+    const maxValue = Math.pow(256, bytesNeeded);
+    let randomNumber;
+    do {
+        const randomBytes = crypto.randomBytes(bytesNeeded);
+        randomNumber = randomBytes.reduce((acc, byte, idx) => acc + byte * Math.pow(256, idx), 0);
+    } while (randomNumber >= maxValue - maxValue % (rangeNumber + 1));
+    return min + randomNumber % (rangeNumber + 1);
 }
